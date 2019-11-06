b4232b279c178f22ad0f24b9b7dd3b70ff9a5c58
Dylan Jhaveri <dylanjhaveri@gmail.com>
2019-11-06 12:43:47 -0800

    remove ad config, unnecessary for this example

--- DemoApp/DemoApp/ViewController.m
+++ DemoApp/DemoApp/ViewController.m
@@ -6,12 +6,8 @@
 - (void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
 
-    JWAdConfig * ad = [JWAdConfig new];
-    ad.tag = @"https://pubads.g.doubleclick.net/gampad/ads?slotname=/124319096/external/ad_rule_samples&sz=640x480&ciu_szs=300x250&cust_params=deployment%3Ddevsite%26sample_ar%3Dpremidpost&url=&unviewed_position_start=1&output=xml_vast3&impl=s&env=vp&gdfp_req=1&ad_rule=0&cue=15000&vad_type=linear&vpos=midroll&pod=2&mridx=1&rmridx=1&ppos=1&lip=true&min_ad_duration=0&max_ad_duration=30000&vrid=6256&video_doc_id=short_onecue&cmsid=496&kfa=0&tfcd=0";
-
     JWConfig * config = [JWConfig new];
     config.file = @"https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8";
-    config.advertising = ad;
     config.autostart = YES;
 
     self.player = [[JWPlayerController alloc] initWithConfig:config];
